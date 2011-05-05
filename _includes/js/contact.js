a8a1dbc90d649d2aac161b659a19c8b2457a59b3
Phillip Ridlen <phillip@balcomagency.com>
2011-05-05 18:00:43 -0500

    adjust effect on contact form and add light yellow background

--- _includes/js/contact.js
+++ _includes/js/contact.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('a#send_email').click(function() {
-    $('#contact').hide().fadeIn(500);
-     document.getElementById('contact').scrollIntoView(true);
+    $('#contact').fadeIn(100);
+    document.getElementById('contact').scrollIntoView(true);
     return false;
   });
   $('#contact #cancel_email').click(function() {
-    $('#contact').fadeOut();
+    $('#contact').slideUp(100);
     return false;
   })
 });
--- js/all.js
+++ js/all.js
@@ -2,7 +2,6 @@
 layout: nil
 ---
 {% include js/jquery.js %}
-{% include js/highlight.js %}
 {% include js/contact.js %}
 {% include js/typekit.js %}
 {% include js/ga.js %}
