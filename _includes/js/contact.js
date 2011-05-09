4b2347ffa68a64b1de12073590c1618e7ba4df6b
Phillip Ridlen <phillip@balcomagency.com>
2011-05-09 12:22:46 -0500

    add thanks message to contact form

--- _includes/js/contact.js
+++ _includes/js/contact.js
@@ -7,5 +7,13 @@ $(document).ready(function() {
   $('#contact #cancel_email').click(function() {
     $('#contact').slideUp(100);
     return false;
-  })
+  });
+  if(location.href.split('?')[1] == "thanks") {
+    $('#notice .content').text("Thanks for your input! I will read but may not necessarily respond to every email.");
+    $('#notice').fadeIn();
+    $('#notice .close a').click(function(){
+      $('#notice').fadeOut();
+      return false;
+    });
+  }
 });
