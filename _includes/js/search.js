ae425700bdc3bae96e4342eddd1b15128e7eb91f
Phillip Ridlen <phillip@ovenbits.com>
2014-04-15 13:34:47 -0500

    More cleanup

--- _includes/js/search.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  $('#search form').submit(function(){
-    $('#query').val("site:blog.phillipridlen.com " + $('#query').val());
-  });
-});
--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,5 @@
 ---
 layout: nil
 ---
-{% include js/jquery.js %}
 {% include js/typekit.js %}
 {% include js/ga.js %}
-{% include js/search.js %}
