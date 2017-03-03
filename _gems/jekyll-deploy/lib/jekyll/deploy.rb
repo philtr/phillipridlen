8d6c4468acdd9f230d70f1c482444c3041ac8e03
Phillip Ridlen <p@rdln.net>
2017-03-03 12:44:51 -0600

    Tag build numbers

--- _gems/jekyll-deploy/lib/jekyll/deploy.rb
+++ _gems/jekyll-deploy/lib/jekyll/deploy.rb
@@ -5,7 +5,8 @@ class Deploy < Jekyll::Command
         c.syntax "deploy"
         c.description 'Deploy site using s3_website'
 
-        c.option "tidy", "--tidy", "Use tidy-html5 to format outputted site"
+        c.option "tidy",      "--tidy",     "Use tidy-html5 to format outputted site"
+        c.option "[no-]tag"   "--[no-]tag"  "Tag this deploy in Git with an incremental build number"
 
         c.action do |args, options|
           Jekyll::Commands::Build.process({})
@@ -15,6 +16,18 @@ class Deploy < Jekyll::Command
               puts "Tidying HTML..."
               system("find _site/ -name '*.html' -exec tidy -config _config/tidy.conf {} \\;")
             end
+          end
+
+          unless options["no-tag"]
+            build = `git tag`
+              .split("\n")
+              .select { |tag| tag =~ /\Ab\d+$\Z/ }
+              .map { |tag| tag[1..-1].to_i }
+              .sort
+              .last
+              .next
+
+            system("git tag b#{build.to_s.rjust(4, "0")}")
           end
 
           system("bundle exec s3_website push", out: $stdout)
