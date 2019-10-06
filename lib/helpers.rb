d56cab05eb916c13032953d0148eca8b936e0562
Phillip Ridlen <p@rdln.net>
2019-10-05 22:09:37 -0500

    Footer and Navigation

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,1 +1,7 @@
 use_helper Nanoc::Helpers::Rendering
+
+NavigationItem = Struct.new(:name, :url, keyword_init: true)
+
+def navigation_items
+  @config[:navigation].map { |nav_data| NavigationItem.new(nav_data) }
+end
