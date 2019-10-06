12b87f773ae14218fb2b73898ec873fb81f17a61
Phillip Ridlen <p@rdln.net>
2019-10-06 00:03:42 -0500

    Working notes index listing

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,7 +1,18 @@
+use_helper Nanoc::Helpers::LinkTo
 use_helper Nanoc::Helpers::Rendering
 
 NavigationItem = Struct.new(:name, :url, keyword_init: true)
 
 def navigation_items
-  @config[:navigation].map { |nav_data| NavigationItem.new(nav_data) }
+  @config[:navigation].map do |nav_data|
+    NavigationItem.new(nav_data)
+  end
+end
+
+def posts_grouped_by_category
+  result = @items.find_all("/posts/**/*")
+    .sort_by { |item| item[:date] - Time.now }
+    .reverse
+    .group_by { |item| item[:category] }
+  result
 end
