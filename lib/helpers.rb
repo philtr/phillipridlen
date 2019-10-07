443a78907428c982491d78d24b52e77141657ad6
Phillip Ridlen <p@rdln.net>
2019-10-06 22:27:04 -0500

    Move in atom.xml blog feed

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,6 +2,17 @@ use_helper Nanoc::Helpers::LinkTo
 use_helper Nanoc::Helpers::Rendering
 
 NavigationItem = Struct.new(:name, :url, keyword_init: true)
+SiteConfig = Class.new(OpenStruct)
+
+def site_config
+  site = SiteConfig.new(@config[:site])
+
+  SiteConfig.new(
+    base_url: ENV["URL"] || ENV["BASE_URL"] || site.base_url,
+    email: ENV["EMAIL"] || site.email,
+    author: ENV["AUTHOR"] || site.author,
+  )
+end
 
 def navigation_items
   @config[:navigation].map do |nav_data|
@@ -9,10 +20,21 @@ def navigation_items
   end
 end
 
-def posts_grouped_by_category
-  result = @items.find_all("/posts/**/*")
-    .sort_by { |item| item[:date] - Time.now }
-    .reverse
+def posts
+  @items.find_all("/posts/**/*")
+end
+
+def posts_sorted_by_date(direction: :desc)
+  posts.sort_by do |item|
+    case direction
+    when :asc then item.fetch(:date) - Time.now
+    when :desc then Time.now - item.fetch(:date)
+    end
+  end
+end
+
+def posts_grouped_by_category(sort: :desc)
+  posts_sorted_by_date(direction: sort)
     .group_by { |item| item[:category] }
-  result
+end
 end
