ed69fbc1cd8948d09f3f626b65eb1ce355f5787e
Phillip Ridlen <p@rdln.net>
2021-04-22 23:49:40 -0500

    Fix photo date in URL

--- rules/photos.rb
+++ rules/photos.rb
@@ -1,20 +1,21 @@
 compile "/photos/**/*.jpg", rep: :original do
-  slug = @item[:title].parameterize
-  write "/photos/#{slug}/#{slug}.jpg"
+  slug = item[:title].parameterize
+  date = item[:date].strftime("%Y/%m")
+  write "/photos/#{date}/#{slug}/#{slug}.jpg"
 end
 
 compile "/photos/**/*.jpg", rep: :medium do
   filter :resize_to_fit, width: 1500
-  slug = @item[:title].parameterize
-
-  write "/photos/#{slug}/#{slug}-medium.jpg"
+  slug = item[:title].parameterize
+  date = item[:date].strftime("%Y/%m")
+  write "/photos/#{date}/#{slug}/#{slug}-medium.jpg"
 end
 
 compile "/photos/**/*.jpg", rep: :thumbnail do
   filter :resize_to_fill, width: 400, height: 400
-  slug = @item[:title].parameterize
-
-  write "/photos/#{slug}/#{slug}-thumbnail.jpg"
+  slug = item[:title].parameterize
+  date = item[:date].strftime("%Y/%m")
+  write "/photos/#{date}/#{slug}/#{slug}-thumbnail.jpg"
 end
 
 
@@ -24,6 +25,8 @@ compile "/photos/**/*.jpg" do
 
   layout "/photo.*"
 
-  write "/photos/#{@item[:title].parameterize}/index.html"
-end
+  slug = item[:title].parameterize
+  date = item[:date].strftime("%Y/%m")
 
+  write "/photos/#{date}/#{slug}/index.html"
+end
