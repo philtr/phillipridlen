86a69e797b0bde4395181d2e5e892a916b40f218
Phillip Ridlen <p@rdln.net>
2025-06-13 12:29:58 -0500

    Add RDoc comments for FreshRSS classes

--- lib/freshrss/config.rb
+++ lib/freshrss/config.rb
@@ -1,11 +1,25 @@
-# Configuration container for the FreshRSS client.
-# Stores connection details such as instance URL and credentials.
-#
-module FreshRSS
-  class Config
-    attr_reader :instance_url, :username, :api_password
-    attr_writer :username, :api_password
+# frozen_string_literal: true
 
+module FreshRSS
+  # Configuration container for the FreshRSS client.
+  # Stores connection details such as instance URL and credentials.
+  class Config
+    # URL of the FreshRSS instance.
+    attr_reader :instance_url
+    # Username used for API requests.
+    attr_reader :username
+    # API password for the user.
+    attr_reader :api_password
+
+    # Set the username.
+    attr_writer :username
+    # Set the API password.
+    attr_writer :api_password
+
+    # Initialize a new configuration object.
+    #
+    # @param attrs [Hash] optional attributes
+    # @yieldparam config [Config] yields itself for configuration
     def initialize(attrs = {})
       self.instance_url = attrs[:instance_url] || attrs["instance_url"]
       self.username = attrs[:username] || attrs["username"]
@@ -14,6 +28,10 @@ module FreshRSS
       yield self if block_given?
     end
 
+    # Set the URL of the FreshRSS instance.
+    # Trailing slashes are removed.
+    #
+    # @param instance_url [String]
     def instance_url=(instance_url)
       @instance_url = instance_url&.chomp("/")
     end
