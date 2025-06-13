d6ad952feaecb90106b8c23434a92e04ecc0bd6c
Phillip Ridlen <p@rdln.net>
2025-06-13 09:51:31 -0500

    Cleaner FreshRSS client config

--- lib/freshrss/client.rb
+++ lib/freshrss/client.rb
@@ -4,19 +4,29 @@ require "uri"
 
 module FreshRSS
   class Client
-    def initialize(base_url:, username:, api_password:)
-      @base_url = base_url.chomp('/')
-      @username = username
-      @api_password = api_password
+    attr_reader :config
+
+    def initialize(config = nil, instance_url: nil, username: nil, api_password: nil)
+      if config
+        @config = config
+      else
+        @config = FreshRSS::Config.new()
+      end
+
+      @config.instance_url = instance_url if instance_url && !instance_url.empty?
+      @config.username = username if username && !username.empty?
+      @config.api_password = api_password if api_password && !api_password.empty?
+
+      yield @config if block_given?
     end
 
     def starred(limit: 50)
-      uri = URI("#{@base_url}/api/greader.php/reader/api/0/stream/contents/user/-/state/com.google/starred")
+      uri = URI("#{config.instance_url}/api/greader.php/reader/api/0/stream/contents/user/-/state/com.google/starred")
       params = { n: limit, output: 'json' }
       uri.query = URI.encode_www_form(params)
 
       req = Net::HTTP::Get.new(uri)
-      req.basic_auth(@username, @api_password)
+      req.basic_auth(config.username, config.api_password)
 
       Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
         res = http.request(req)
---
+++ lib/freshrss/config.rb
@@ -0,0 +1,17 @@
+module FreshRSS
+  class Config
+    attr_accessor :instance_url, :username, :api_password
+
+    def initialize(attrs = {})
+      self.instance_url = attrs[:instance_url] || attrs['instance_url']
+      self.username = attrs[:username] || attrs['username']
+      self.api_password = attrs[:api_password] || attrs['api_password']
+
+      yield self if block_given?
+    end
+
+    def instance_url=(instance_url)
+      @instance_url = instance_url&.chomp('/')
+    end
+  end
+end
