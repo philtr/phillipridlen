545079ad0d6952b7783ff1a23033ed8e7896f30f
Phillip Ridlen <p@rdln.net>
2025-10-29 09:31:46 -0500

    Don't reinitialize constants

--- lib/data_sources/exif/item.rb
+++ lib/data_sources/exif/item.rb
@@ -1,5 +1,4 @@
 require "exiftool_vendored"
-
 require_relative "../nanoc_transformable"
 
 module DataSources
@@ -11,13 +10,6 @@ module DataSources
     include NanocTransformable::Binary
 
     attr_reader :attributes
-
-    # Format for parsing EXIF date strings
-    EXIF_DATE_FORMAT = "%Y:%m:%d %H:%M:%S"
-    # Unit/postfix for "seconds" of exposure
-    SECONDS = "𝑠"
-    # Unit/prefix for F-stop focal length
-    F_STOP = "𝑓/"
 
     def initialize(filename)
       @exif = Exiftool.new(filename).to_hash.freeze
@@ -33,7 +25,7 @@ module DataSources
         # Description or caption
         description: exif(:user_comment),
         # DateTime object parsed from the EXIF string
-        date: parse_exif_date(exif(:date_time_original)),
+        date: exif_date(exif(:date_time_original)),
         # Source filename
         filename: exif(:source_file),
         # Camera make and model
@@ -41,24 +33,24 @@ module DataSources
         # Lens information, with fancy "𝑓"
         lens: replace_f_stop(exif(:lens_info)),
         # F-stop, with fancy "𝑓"
-        f_stop: "#{F_STOP}#{exif(:f_number)}",
+        f_stop: "#{f_stop_prefix}#{exif(:f_number)}",
         # Exposure time in seconds, with fancy "𝑠"
-        exposure: "#{exif(:exposure_time)}#{SECONDS}",
+        exposure: "#{exif(:exposure_time)}#{seconds_unit}",
         # ISO number
         iso: exif(:iso)
       }.transform_values(&:freeze).freeze
     end
 
-    def exif(key)
-      @exif.fetch(key)
-    end
+    def exif(key) = @exif.fetch(key)
 
-    def replace_f_stop(str)
-      str.gsub("f/", F_STOP)
-    end
+    def replace_f_stop(str) = str.gsub("f/", f_stop_prefix)
 
-    def parse_exif_date(exif_date_str)
-      Time.strptime(exif_date_str, EXIF_DATE_FORMAT)
-    end
+    def exif_date(str) = Time.strptime(str, "%Y:%m:%d %H:%M:%S")
+
+    private
+
+    def seconds_unit = "𝑠"
+
+    def f_stop_prefix = "𝑓/"
   end
 end
