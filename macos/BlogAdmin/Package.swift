10573b33d06e752b3438061e5cabe9035416ab6e
aoeu <p@rdln.net>
2025-12-28 16:21:11 -0600

    Improve text editor

--- macos/BlogAdmin/Package.swift
+++ macos/BlogAdmin/Package.swift
@@ -9,12 +9,16 @@ let package = Package(
     .executable(name: "BlogAdmin", targets: ["BlogAdmin"])
   ],
   dependencies: [
-    .package(url: "https://github.com/jpsim/Yams.git", from: "6.2.0")
+    .package(url: "https://github.com/jpsim/Yams.git", from: "6.2.0"),
+    .package(url: "https://github.com/swiftlang/swift-markdown.git", from: "0.4.0")
   ],
   targets: [
     .executableTarget(
       name: "BlogAdmin",
-      dependencies: ["Yams"]
+      dependencies: [
+        "Yams",
+        .product(name: "Markdown", package: "swift-markdown")
+      ]
     )
   ]
 )
