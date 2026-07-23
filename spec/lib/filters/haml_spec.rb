ad4533676dd7d66d60a091c1df1616277ef7801c
Phillip Ridlen <p@rdln.net>
2026-07-23 08:20:36 -0500

    fix: broken spec

--- spec/lib/filters/haml_spec.rb
+++ spec/lib/filters/haml_spec.rb
@@ -3,7 +3,7 @@ require "filters/haml"
 RSpec.describe NanocFilters::Haml do
   it "renders Haml templates" do
     filter = described_class.new(name: "World")
-    result = filter.setup_and_run(%(%p= "Hello #{name}"))
+    result = filter.setup_and_run("%p= \"Hello \#{name}\"")
     expect(result.strip).to eq("<p>Hello World</p>")
   end
 end
